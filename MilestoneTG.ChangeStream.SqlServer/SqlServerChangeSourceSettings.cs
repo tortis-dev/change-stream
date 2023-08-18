f9a446f02fd491da417f0784582a439381909ada
Jason Penniman <jpenniman@gmail.com>
2023-08-18 11:06:01 -0400

    cleaned up settings dependencies

---
+++ MilestoneTG.ChangeStream.SqlServer/SqlServerChangeSourceSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace MilestoneTG.ChangeStream.SqlServer;
+
+public class SourceSettings
+{
+    public string ConnectionString { get; set; } = string.Empty;
+    public string SchemaName { get; set; } = string.Empty;
+    public string TableName { get; set; } = string.Empty;
+    public int IntervalInMilliseconds { get; set; } = 1000;
+}
